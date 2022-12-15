87cd348da28be9dd4d1088f95b0f4ef8354a9b36
ypolosov <ypolosov@gmail.com>
2022-12-15 13:47:00 +0000

    sdfsdf

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker -H "ssh://$SSH_USER@$SSH_HOST:$SSH_PORT" run --rm -d --net host -e "APP_PORT=3000" -e "APP_NAME=app-1" "$SSH_USER/app-1"
+docker -H "ssh://$SSH_USER@$SSH_HOST:$SSH_PORT" run --rm -d --net host -e "APP_PORT=3000" -e "APP_NAME=app-1" "$SSH_USER/app-1:0.1.0"
